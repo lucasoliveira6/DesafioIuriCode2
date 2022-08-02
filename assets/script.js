34082902671adf8f39921438bc981d83725608f4
Lucas <lucas.oliveira13400@gmail.com>
2022-08-02 19:36:57 -0300

    Desafio IuriCode2

---
+++ assets/script.js
@@ -0,0 +1,14 @@
+ const sr = ScrollReveal({
+            origin: 'top',
+            distance: '50px',
+            duration: 1500,
+            reset: true
+        });
+
+        ScrollReveal().reveal('.card', { delay: 200 });
+
+
+var typed = new Typed('#typed-text', {
+    strings: ["O tênis Jordan é fruto de uma velha e forte parceria entre a Nike e o jogador Michael Jordan."],
+    typeSpeed: 30,
+  });
